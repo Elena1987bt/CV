a2e129be0b046798d221f1bcfd72b82874c98a43
Elena Ackovska <ackove01@stepstone.com>
2023-10-06 13:29:27 +0200

    added cv pdf

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -6,9 +6,7 @@ const References = () => {
       <h1 className="text-lg my-2 font-semibold text-gray-900 ">Reference</h1>
       <div className="flex justify-betweens">
         <div className="mr-20">
-          <h2 class="mb-1 text-xl font-semibold leading-none text-gray-900 dark:text-gray-500">
-            Niclas Bjuväng
-          </h2>
+          <h2 class="mb-1 text-xl font-semibold leading-none text-gray-900 dark:text-gray-500"></h2>
           <p className="pb-3 text-justify text-gray-900 ">
             Vice president of engineering at StepStone
           </p>
@@ -24,9 +22,7 @@ const References = () => {
           </p>
         </div>
         <div>
-          <h2 class="mb-1 text-xl font-semibold leading-none text-gray-900 dark:text-gray-500">
-            Matthew Lawson
-          </h2>
+          <h2 class="mb-1 text-xl font-semibold leading-none text-gray-900 dark:text-gray-500"></h2>
           <p className="pb-3 text-justify text-gray-900 ">
             Principal developer at Google
           </p>
